24b16219867af7b5e40f8b9083d70d5ad92c5996
md arshad <arshadansari14714@gmail.com>
2025-04-10 21:44:21 +0530

    Fix static file setup with whitenoise

--- my_website/settings.py
+++ my_website/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
 ]
 
 ROOT_URLCONF = 'my_website.urls'
@@ -123,6 +124,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
