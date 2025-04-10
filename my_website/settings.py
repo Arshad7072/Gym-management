2230a625bb7f503f44f2a6f1957e962a4f73267c
md arshad <arshadansari14714@gmail.com>
2025-04-10 21:11:43 +0530

    add render.com domain to allowed_hosts

--- my_website/settings.py
+++ my_website/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-l(agu#k66@ww)@kzku=0@a$dhqm@iawm)k&e_3w^=cu@)*=)#)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['gym-management-5-3q2y.onrender.com']
 
 
 
